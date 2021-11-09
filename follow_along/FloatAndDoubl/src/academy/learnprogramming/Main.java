9f7f838bca5d41d88a00baad0fd82c23955ef0b3
Larry Olguin <iraqwarvet31@gmail.com>
2021-11-09 15:23:17 -0800

    add exercises

---
+++ follow_along/FloatAndDoubl/src/academy/learnprogramming/Main.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int lbs = 200;
+        double result = lbs / 2.2046;
+        System.out.println(lbs + " lbs = " + result + " kilograms.");
+    }
+}
